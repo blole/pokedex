3b7771f3e51457a5807df33a117a457489118204
Björn Holm <blollle@gmail.com>
2024-02-13 20:25:52 +0100

    add search parsing

---
+++ apps/app0/src/search/searchStringParser.ts
@@ -0,0 +1,51 @@
+import nearley from "nearley";
+import grammar, { type Node } from "./grammar";
+import { Result, err, ok } from "@/util/result";
+
+export type UnexpectedAtOffset = {
+  code: "UnexpectedAtOffset";
+  offset: number;
+};
+
+export type UnexpectedEndOfInput = {
+  code: "UnexpectedEndOfInput";
+};
+
+type ParserError = {
+  offset: number;
+};
+
+const isParserError = (error: unknown): error is ParserError => {
+  return !!(
+    error &&
+    typeof error === "object" &&
+    "offset" in error &&
+    typeof error.offset === "number"
+  );
+};
+
+export const parseSearchString = (
+  search: string,
+): Result<Node, UnexpectedAtOffset | UnexpectedEndOfInput> => {
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+  try {
+    parser.feed(search);
+    const results = parser.results as Node[];
+
+    if (results.length > 1) {
+      // alert the developers
+      throw Error(`ambiguous grammar! results: ${JSON.stringify(results)}`);
+    }
+
+    if (results.length != 1) {
+      return err({ code: "UnexpectedEndOfInput" });
+    }
+
+    return ok(results[0]);
+  } catch (error) {
+    if (isParserError(error)) {
+      return err({ code: "UnexpectedAtOffset", offset: error.offset });
+    }
+    throw error;
+  }
+};

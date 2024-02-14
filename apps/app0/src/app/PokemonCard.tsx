687c7ea95c8bd5fd72357eb696af9e62eee4501f
Björn Holm <blollle@gmail.com>
2024-02-14 20:37:01 +0100

    add a search bar

--- apps/app0/src/app/PokemonCard.tsx
+++ apps/app0/src/app/PokemonCard.tsx
@@ -8,13 +8,13 @@ export type PokemonCardProps = {
 export const PokemonCard = ({ id, name }: PokemonCardProps) => {
   return (
     <div className="group">
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-800">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-sky-800 dark:bg-sky-950">
         <Image
           className="h-full w-full object-cover object-center group-hover:opacity-60"
           src={`/sprites/${id}.png`}
           alt={`${name} art`}
-          width={180}
-          height={37}
+          width={475}
+          height={475}
           priority
         />
       </div>
---
+++ apps/app0/src/app/Search.tsx
@@ -0,0 +1,43 @@
+import React, { ChangeEvent, KeyboardEvent, useCallback } from "react";
+
+export type SearchProps = {
+  value: string;
+  setValue: (value: string) => void;
+  onSubmit: (value: string) => void;
+  className?: string;
+};
+
+export const Search = ({
+  value,
+  setValue,
+  onSubmit,
+  className,
+}: SearchProps) => {
+  const onChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target?.value);
+    },
+    [setValue],
+  );
+
+  const onKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+        onSubmit(event.currentTarget.value);
+      }
+    },
+    [onSubmit],
+  );
+
+  return (
+    <input
+      type="search"
+      placeholder="Search"
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      className={className}
+    />
+  );
+};
